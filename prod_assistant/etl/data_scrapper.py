013a08ad7b661aaec4a895754fd668fa0b4d4fce
Abbasi <suel.abbasi@shell.com>
2025-08-31 16:13:33 +0530

    added astradb cred

--- prod_assistant/etl/data_scrapper.py
+++ prod_assistant/etl/data_scrapper.py
@@ -6,7 +6,7 @@ import time
 import re 
 import os 
 from bs4 import BeautifulSoup
-import undetected_chromedriver as uc 
+import undetected_chromedrive as uc 
 from selenium.webdriver.common.by import By 
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
